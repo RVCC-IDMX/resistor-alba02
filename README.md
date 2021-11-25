a8e048a84cc3ee410af07e170dc5267b1912aa3f
alba02 <naomimaldo02@gmail.com>
2021-11-24 19:55:37 -0500

    Add resistor code and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Resistor
+# Resistor Calculator
+
+## This resistor calculator calculates the resitor value and the electrical resistance. When a color is clicked for each band the value calculates and the bands change colors.
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 import { getResistorOhms } from './resistor.js';
+
+
+//query selectors for band colors + answer  
+const answer = document.querySelector('.answer');
+const color0 = document.querySelector('#color0');
+const color1 = document.querySelector('#color1');
+const color2 = document.querySelector('#color2');
+const color3 = document.querySelector('#color3');
+
+
+// object for bands colors;
+const bands = {
+    color1:'red',
+    color2:'green',
+    multiplier:'violet',
+    tolerance:'gold'
+}
+
+
+
+//query selector for the 4 bands;
+const b0 = document.querySelector('#b0');
+const b1 = document.querySelector('#b1')
+const b2 = document.querySelector('#b2')
+const b3 = document.querySelector('#b3')
+
+let color;
+//event listeners for color 0
+
+color0.addEventListener('click', function(e){
+ color = e.target.className.replace("box ", " ");
+b0.className = `band ${color}`;
+bands.color1 = color;
+answer.innerText= getResistorOhms(bands);
+})
+
+//color 1
+
+color1.addEventListener('click', function(e){
+    color = e.target.className.replace("box ", " ");
+   b1.className = `band ${color}`;
+   bands.color2 = color;
+   answer.innerText= getResistorOhms(bands);
+   })
+//color2
+   color2.addEventListener('click', function(e){
+    color = e.target.className.replace("box ", " ");
+   b2.className = `band ${color}`;
+   bands.multiplier = color;
+   answer.innerText= getResistorOhms(bands);
+   })
+//color 3
+   color3.addEventListener('click', function(e){
+    color = e.target.className.replace("box ", " ");
+   b3.className = `band ${color}`;
+   bands.tolerance = color;
+   answer.innerText= getResistorOhms(bands);
+   })
